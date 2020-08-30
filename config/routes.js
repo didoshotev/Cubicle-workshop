fa392f8720c9a716e3e537c64f60776f93f2950b
Didoshotev <didoshotev@gmail.com>
2020-08-30 18:42:13 +0300

    Create a cube functionallity, added it in db, fixed Document titles

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,51 @@
 // TODO: Require Controllers...
+const express = require('express');
+const Cube = require('../models/cube');
+const {createCube} = require('../controllers/createCube');
 
 module.exports = (app) => {
-        app.get('/', (req, res) => {
-                res.render('index');
-        });
-        app.get('/about', (req, res) => {
-                res.render('about');
-        });
-        app.get('/create', (req, res) => {
-                res.render('create');
-        });
-        app.use('*', (req, res) => {
-                res.render('404');
-        });
+	app.get('/', (req, res) => {
+		res.render('index', {
+			"title": 'Home | Cubicle'
+		});
+	});
+	app.post('/create', (req, res) => {
+		const {
+			name,
+			description,
+			imageUrl,
+			difficultyLevel,
+		} = req.body;
+		if(!name){
+			alert('Name Field is neccessary!');
+		};
+		createCube({
+			name,
+			description,
+			imageUrl,
+			difficultyLevel,
+		});
+		res.redirect('/');
+	});
+	app.get('/about', (req, res) => {
+		res.render('about', {
+			"title": 'About | Cubicle'
+		});
+	});
+	app.get('/create', (req, res) => {
+		res.render('create', {
+			"title": 'Create | Cubicle'
+		});
+	});
+	app.get('/details:id', (req, res) => {
+		res.render('details', {
+			"title": 'Details | Cubicle'
+			//add id
+		});
+	});
+	app.use('*', (req, res) => {
+		res.render('404', {
+			"title": 'Error 404'
+		});
+	});
 };
--- controllers/createCube.js
+++ controllers/createCube.js
@@ -1,3 +1,14 @@
 const Cube = require('../models/cube');
-const a = new Cube('Default', 'This is default cube', 'https://google.com', 1);
-a.save();
+
+const createCube = (data) => {
+    const { name, description, imageUrl, difficultyLevel } = data;
+    const newCube = new Cube(name, description, imageUrl, difficultyLevel);
+    newCube.save();
+};
+
+module.exports = {
+    createCube,
+}
+
+
+//createCube('Default', 'This is default cube', 'https://google.com', 1)
