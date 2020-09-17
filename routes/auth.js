4a05597e52cc21767e710e1cdffe59fed655bf3a
Didoshotev <didoshotev@gmail.com>
2020-09-17 22:49:26 +0300

    feat/new routes, hashed the password, jwt token for each session

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,7 @@
+const User = require('../models/user');
+const userController = require('../controllers/user');
+const { static } = require('express');
+
 module.exports = (app) => {
     app.get('/login', (req, res) => {
         res.render('loginPage', {
@@ -8,5 +12,13 @@ module.exports = (app) => {
         res.render('registerPage', {
             "title": 'Register | Cubicle',
         })
-    })	
+    })
+
+    app.post('/register', async (req, res) => {
+        const status = await userController.saveUser(req, res);
+        if(status === true){
+            res.redirect('/')
+        }
+       
+    })
 };
